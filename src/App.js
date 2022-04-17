059f9eb7c6fb90815f9e45063b30aba817ad1a7f
Evelina Staniulytė <evelina.staniulyte@unity3d.com>
2022-04-17 13:06:08 +0300

    Clear page & add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './routes';
+import React from 'react';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename="/react-particles">
+      <div className="App-header">
+        <main className="content">
+          <Router />
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
