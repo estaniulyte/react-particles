c71481c9f66c663aab4e741291ab6391971afb64
Evelina Staniulytė <evelina.staniulyte@unity3d.com>
2022-04-17 13:17:13 +0300

    Add Home page

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import AbsorbersParticles from 'components/AbsorbersParticles';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main>
+      main section
+      <AbsorbersParticles />
+    </main>
+  );
+};
+
+export default Home;
